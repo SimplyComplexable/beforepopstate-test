67edb602ecb4213920b05148e03d8e6e40a2a8b8
zsunderland <zsunderland@traeger.com>
2022-01-24 15:18:12 -0700

    Add beforePopState logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
+import {useRouter} from "next/router";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const setIsOpen = () => {
+    alert('hello');
+  }
+  React.useEffect(() => {
+    router.push('/', '/asdf', { shallow: true });
+    router.beforePopState(() => {
+        setIsOpen(true);
+      return true;
+    });
+
+  }, [setIsOpen, router.route]);
   return <Component {...pageProps} />
 }
 
